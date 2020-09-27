9657128c79cc7672f344ba54330e207ac2bc7a82
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 10:58:18 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:58:18 up  3:10,  1 user,  load average: 0.34, 0.19, 0.07 b6f60be5856008b23beae346e96534dd9c48806

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,8 +14,8 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef struct {
-	union{	
+typedef struct {	
+	union {
 		union {
 			uint32_t _32;
 			uint16_t _16;
@@ -28,6 +28,7 @@ typedef struct {
 			uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 		};
 	};
+
 	swaddr_t eip;
 
 } CPU_state;
