d34bcd1a9a61c5868fe94bebbc4a2d9d01253ac0
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 08:48:45 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:48:45 up  1:00,  1 user,  load average: 0.87, 0.42, 0.26 e07ffc396063c4932a636843eeb58e72fd7837e

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -19,9 +19,9 @@ typedef struct {
 		union{
 		uint32_t _32;
 		uint16_t _16;
-		uint8_t _8[2];
+		uint8_t _8[8];
 		};
-	} gpr[8];
+	} gpr[2];
 
 	/* Do NOT change the order of the GPRs' definitions. */
 
