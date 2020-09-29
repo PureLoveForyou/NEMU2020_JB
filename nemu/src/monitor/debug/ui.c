ac1aab8e8cb6dac37fecd4abeeca8f820154c129
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-29 13:48:49 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:48:49 up  2:24,  1 user,  load average: 0.26, 0.16, 0.16 75e809907f3c8a11ed3280d93387c687d2ae1d90

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -110,13 +110,13 @@ static int cmd_info(char *args)
 	else if(*arg == 'r'){
 		int i;
 		for(i = 0; i < 8; i++) {
-			printf("%s\t0x%8x\t%d\n", regsl[i], cpu.gpr[i]._32, cpu.gpr[i]._32);
+			printf("%s\t0x%08x\t%d\n", regsl[i], cpu.gpr[i]._32, cpu.gpr[i]._32);
 		}
 		for(i = 0; i < 8; i++) {
-			printf("%s\t0x%8x\t%d\n", regsw[i], cpu.gpr[i]._16, cpu.gpr[i]._16);
+			printf("%s\t0x%08x\t%d\n", regsw[i], cpu.gpr[i]._16, cpu.gpr[i]._16);
 		}
 		for(i = 0; i < 8; i++) {
-			printf("%s\t0x%4x%4x\t%d%d\n", regsb[i], cpu.gpr[i]._8[0], cpu.gpr[i]._8[1], cpu.gpr[i]._8[0], cpu.gpr[i]._8[1]);
+			printf("%s\t0x%04x%04x\t%d%d\n", regsb[i], cpu.gpr[i]._8[0], cpu.gpr[i]._8[1], cpu.gpr[i]._8[0], cpu.gpr[i]._8[1]);
 		}
 	}
 	else {
