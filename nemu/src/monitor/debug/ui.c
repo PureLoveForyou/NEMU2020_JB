9571ffa57e3c16cfe5fa4c710f88064d32300c9e
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-29 13:52:55 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:52:55 up  2:28,  1 user,  load average: 0.21, 0.18, 0.16 71ad4fa06f55359c7035ce7012e02ed5d049cdeb

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -116,7 +116,7 @@ static int cmd_info(char *args)
 			printf("%s\t0x%08x\t%d\n", regsw[i], cpu.gpr[i]._16, cpu.gpr[i]._16);
 		}
 		for(i = 0; i < 8; i++) {
-			printf("%s\t0x%04x%04x\t%d%d\n", regsb[i], cpu.gpr[i]._8[0], cpu.gpr[i]._8[1], cpu.gpr[i]._8[0], cpu.gpr[i]._8[1]);
+			printf("%s\t0x%06x%x\t%d%d\n", regsb[i], cpu.gpr[i]._8[1], cpu.gpr[i]._8[0], cpu.gpr[i]._8[1], cpu.gpr[i]._8[0]);
 		}
 	}
 	else {
