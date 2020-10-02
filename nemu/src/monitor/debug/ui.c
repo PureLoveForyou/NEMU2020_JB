ebc0c67b81266a7a7ee88c6a91102da5cc82042e
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-02 03:22:10 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:22:10 up 41 min,  1 user,  load average: 0.08, 0.15, 0.17 d121e23b6693b4bff445ab4aef06fb8155815dec

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -149,7 +149,7 @@ static int cmd_x(char *args)
 		for(i = 0; i < num; i++) {
 			//printf("NOthing yet\n");
 			content = swaddr_read(VirtualAddress + i*4, 4);
-			printf("%-8x\n", content);
+			printf("%-80x\n", content);
 		}
 	}
 	return 0;
