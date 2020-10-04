1b9fad0fcbeea74151aca6babbd4659cd142bf35
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 15:46:13 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:46:13 up  1:31,  1 user,  load average: 0.09, 0.19, 0.18 fbbe356461f2e4eb5cb94e11d9f37632ce32f38f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -167,7 +167,7 @@ static int eval(int p, int q) {
 		int i;
 		result = 0;
 		for(i = 0; i < 32; i++) {
-			result = result*10 + tokens[p].str[i] - '0';
+			result = result*10 + (int)(tokens[p].str[i] - '0');
 		}
 	}
 	else if(check_parentheses(p, q) == true) {
