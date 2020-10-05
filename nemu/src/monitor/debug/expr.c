8f2e8a73fe58ce37eb4729740811f5be0343b264
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-05 06:23:21 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:23:21 up 16:08,  1 user,  load average: 0.23, 0.15, 0.06 64b452cf8d51c9c9deca56966dbd55da64f2553

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -29,7 +29,7 @@ static struct rule {
 	{"/", '/'},					//divide
 	{"\\(", '('},					//left bracket
 	{"\\)", ')'},					//right bracket
-	{"[1-9][0-9]*", '1'}				//number
+	{"[0-9][0-9]*", '1'}				//number
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
