7951c4e46d1ef90deb28b284d570319d782d59db
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-09 13:03:41 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:03:41 up  5:31,  1 user,  load average: 0.19, 0.27, 0.25 19691d682c50cfaee5f4a70c13a2ad442349d18

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -114,11 +114,24 @@ static bool make_token(char *e) {
 					case DOLREG:	tokens[nr_token].type = rules[i].token_type;
 							int y;
 							for(y = 0; y < 32; y++) {
-								tokens[nr_token].str[y] = ' ';
+								tokens[nr_token].str[y] = '\0';
 								if(y <= substr_len -2)
 									tokens[nr_token].str[y] = substr_start[y+1];
 							}
-							tokens[nr_token].str[substr_len - 1] = '\0';
+							for(y = 0; y < 8; y++) {
+								if(strcmp(tokens[nr_token].str, regsl[y]) == 0) {
+									tokens[nr_token].str[4] = y;break;
+								}
+								else if(strcmp(tokens[nr_token].str, regsw[y]) == 0) {
+									tokens[nr_token].str[5] = y;break;
+								}
+								else if(strcmp(tokens[nr_token].str, regsb[y]) == 0) {
+                                                                        tokens[nr_token].str[6] = y;break;
+                                                                }
+								else if(strcmp(tokens[nr_token].str, "eip") == 0) {
+                                                                        tokens[nr_token].str[7] = y;break;
+                                                                }
+							}
 							nr_token++;break;
 					case HEXNUM: 	tokens[nr_token].type = rules[i].token_type; 
 							int k, l;
@@ -206,13 +219,17 @@ static uint32_t eval(int p, int q) {
 			}
 		}
 		else if(tokens[p].type == DOLREG) {
-				int size = (int)strlen(tokens[p].str);
-				int flag = 0;
-				if(tokens[p].str == regsl[0]) flag = 1;
-				printf("flag:%d", flag);
-				printf("size:%d str:%s reg:%s\n", size, tokens[p].str, regsl[0]);
-                                assert(0);
-                        }	
+			int index;
+			for(i = 4; i < 8 && tokens[p].str[i] != '\0'; i++);
+			index = tokens[p].str[i];
+			switch(i) {
+				case 4:	return reg_l(index);
+				case 5: return reg_w(index);
+				case 6: return reg_b(index);
+				case 7: return reg_l(index);
+				default:assert(0);
+			}
+		}	
 		else {
 			/*The number is a hexadecimal number*/
 			result = 0;
