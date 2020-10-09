95ae422bfe5a814aa1b7160f6461b866343fdb27
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-09 17:49:14 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:49:14 up  1:00,  1 user,  load average: 0.14, 0.11, 0.15 8f7ba43b59c1e2ecaaccefa4dfaa22b022056833

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -225,7 +225,7 @@ static uint32_t eval(int p, int q) {
 			/*Access registers*/
 			int index;
 			for(i = 4; i < 8 && tokens[p].str[i] != '\0'; i++);
-			index = tokens[p].str[i];
+			index = tokens[p].str[i] - '0';
 			printf("index: %d\n", index);
 			switch(i) {
 				case 4:	return reg_l(index);
