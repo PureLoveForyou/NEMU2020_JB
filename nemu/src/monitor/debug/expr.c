f691832b4f9b0185de65eb5b5b6871178ea2c8d4
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-09 11:50:51 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:50:51 up  4:18,  1 user,  load average: 0.18, 0.22, 0.18 e48699ced622f6febada72f22fcb51d6eff6fe

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -206,14 +206,8 @@ static uint32_t eval(int p, int q) {
 			}
 		}
 		else if(tokens[p].type == DOLREG) {
-                                int i;
-				char* reg = tokens[p].str;
-				for(i = 0; i < 32 && i != '\0'; i++)
-					reg[i] = tokens[p].str[i];
-				reg[i] = '\0';
-                                for(i = 0; i < 8 && reg != regsl[i] && reg != regsw[i] && reg != regsb[i]; i++);
-                                if(reg == regsl[i])
-                                        printf("%s\n", regsl[i]);
+				int size = (int)strlen(tokens[p].str);
+				printf("%d\n", size);
                                 assert(0);
                         }	
 		else {
