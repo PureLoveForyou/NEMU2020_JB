eb63e306b8dad53a47e485f8aff0e757f5776c53
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-10 08:38:52 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:38:52 up  2:39,  1 user,  load average: 0.88, 0.38, 0.20 6dc87ff18905cafbd61e8c2fce053ab860104a38

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -140,7 +140,7 @@ static int cmd_info(char *args)
 static int cmd_x(char *args)
 {
 	char *arg1 = strtok(NULL, " ");
-	char *arg2 = strtok(NULL, "");
+	char *arg2 = strtok(NULL, " ");
 	int num, i;
 	bool success = 1;
 	int VirtualAddress,content;
