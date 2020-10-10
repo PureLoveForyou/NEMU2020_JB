56f08b42d0467b3c1599112219bf61de9a268967
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-10 00:34:23 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:34:23 up 1 min,  1 user,  load average: 0.86, 0.37, 0.14 8ede37d52d751945fa8cb96af200a1594329cf31

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -122,16 +122,16 @@ static bool make_token(char *e) {
 							printf("reg:%s\n", tokens[nr_token].str);
 							for(y = 0; y < 8; y++) {
 								/*Find out which register it is and record it's position*/
-								if(strcmp(tokens[nr_token].str, regsl[y]) == 0) {
+								if(strcmp(tokens[nr_token].str, regsl[y])==0) {
 									tokens[nr_token].str[4] = y + '0';printf("hello");break;
 								}
-								else if(strcmp(tokens[nr_token].str, regsw[y]) == 0) {
+								else if(strcmp(tokens[nr_token].str, regsw[y])==0) {
 									tokens[nr_token].str[5] = y + '0';break;
 								}
-								else if(strcmp(tokens[nr_token].str, regsb[y]) == 0) {
+								else if(strcmp(tokens[nr_token].str, regsb[y])==0) {
                                                                         tokens[nr_token].str[6] = y + '0';break;
                                                                 }
-								else if(strcmp(tokens[nr_token].str, "eip") == 0) {
+								else if(strcmp(tokens[nr_token].str, "eip") ==0) {
                                                                         tokens[nr_token].str[7] = y + '0';break;
                                                                 }
 							}
