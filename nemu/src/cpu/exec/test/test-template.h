fa58648b666ae0ec4376dd1589bf35a2bc62c5f9
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-29 16:08:36 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:08:36 up 8 days,  2:59,  1 user,  load average: 0.89, 0.64, 0.59 ee3c38f46805b92ed3b400741ebb17ddf82437d

--- nemu/src/cpu/exec/test/test-template.h
+++ nemu/src/cpu/exec/test/test-template.h
@@ -3,7 +3,18 @@
 #define instr test
 
 static void do_execute() {
-    
+    DATA_TYPE andresult = op_src->val & op_dest->val;
+
+    cpu.CF = 0;
+    cpu.ZF = !andresult;
+    cpu.OF = 0;
+    cpu.SF = (andresult >> 31) & 0x1;//get sign flag
+
+    /*judge whether number of 1 in low 8 bits is even*/
+    andresult ^= andresult >> 4;
+    andresult ^= andresult >> 2;
+    andresult ^= andresult >> 1;
+    cpu.PF = !andresult;
 }
 
 make_instr_helper(r2rm);
