d2af898f5be3f6ecb9a7111fd097836a3518dd41
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 13:42:08 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:42:08 up  2:39,  1 user,  load average: 0.78, 0.49, 0.58 d70ce69828ed3a1e31339e6f80fb93bdfcb7da05

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -32,7 +32,8 @@ make_helper(concat(decode_si_, SUFFIX)) {
 	 *
 	op_src->simm = ???
 	 */
-	panic("please implement me");
+	//panic("please implement me");
+	op_src->simm = instr_fetch(eip, DATA_BYTE);
 
 	op_src->val = op_src->simm;
 
