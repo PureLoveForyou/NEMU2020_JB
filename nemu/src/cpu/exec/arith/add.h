53e43c5d39a982375dd2d1507ac7020485217890
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-04 15:40:36 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:40:36 up 5 days,  4:37,  1 user,  load average: 1.35, 1.11, 0.80 4f4f9c5351744b1775dc5cb5ceacd480475a1aad

--- nemu/src/cpu/exec/arith/add.h
+++ nemu/src/cpu/exec/arith/add.h
@@ -2,6 +2,15 @@
 #define _ADD_H_
 
 make_helper(add_si2rm_v);
+make_helper(add_r2rm_v);
+make_helper(add_i2a_v);
+make_helper(add_rm2r_v);
+make_helper(add_i2rm_v);
+
 make_helper(add_si2rm_b);
+make_helper(add_r2rm_b);
+make_helper(add_i2a_b);
+make_helper(add_rm2r_b);
+make_helper(add_i2rm_b);
 
 #endif
