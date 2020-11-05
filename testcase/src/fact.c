aca14c8798b3c3222e99fd9e4e0fc6dc612fb3b6
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-05 14:28:18 +0000

    >  run 3019244235 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:28:18 up 6 days,  3:25,  1 user,  load average: 0.81, 0.79, 0.89 852fb454bd8025f1276875c079f8c510cb70428e

--- testcase/src/fact.c
+++ testcase/src/fact.c
@@ -10,12 +10,12 @@ int fact(int n) {
 
 int main() {
 	int i;
-	for(i = 0; i < 13; i ++) {
+	for(i = 0; i < 7; i ++) {
 		f[i] = fact(i);
 		nemu_assert(f[i] == ans[i]);
 	}
 
-	nemu_assert(i == 13);
+	nemu_assert(i == 7);
 
 	return 0;
 }
