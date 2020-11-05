387a01d07305fe2ad792155c697b03fad547ffc0
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-05 09:14:59 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:14:59 up 5 days, 22:12,  1 user,  load average: 0.80, 0.62, 0.66 c7d311ebaf291fa5b301470f3ecf0fe2f72839

--- nemu/src/cpu/exec/logic/shl-template.h
+++ nemu/src/cpu/exec/logic/shl-template.h
@@ -12,17 +12,17 @@ static void do_execute () {
 
 	/* TODO: Update EFLAGS. */
 	//panic("please implement me");
-	//DATA_TYPE_S result = dest;
-	//cpu.CF = MSB(src);
-    //cpu.ZF = !result;
-    //cpu.OF = 0;
-    //cpu.SF = MSB(result);//get sign flag
+	DATA_TYPE_S result = dest;
+	cpu.CF = MSB(src);
+    cpu.ZF = !result;
+    cpu.OF = 0;
+    cpu.SF = MSB(result);//get sign flag
 
     /*judge whether number of 1 in low 8 bits is even*/
-    //result ^= result >> 4;
-    //result ^= result >> 2;
-    //result ^= result >> 1;
-    //cpu.PF = !(result & 1);
+    result ^= result >> 4;
+    result ^= result >> 2;
+    result ^= result >> 1;
+    cpu.PF = !(result & 1);
 
 	print_asm_template2();
 }
