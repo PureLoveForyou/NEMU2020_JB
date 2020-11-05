16202da027e8ca81197534e77e443dd75dbeb6c6
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-05 16:19:26 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:19:26 up 6 days,  5:16,  1 user,  load average: 0.56, 0.68, 0.83 725b4eed4f1714f19f24728a6121deb1cae3acfa

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -83,7 +83,7 @@ make_group(group5,
 	inv, inv, push_rm_v, inv)
 
 make_group(group6,
-	inv, inv, inv, inv, 
+	inv, inv, inv, inv,
 	inv, inv, inv, inv)
 
 make_group(group7,
--- nemu/src/cpu/exec/logic/and-template.h
+++ nemu/src/cpu/exec/logic/and-template.h
@@ -3,7 +3,7 @@
 #define instr and
 
 static void do_execute () {
-	DATA_TYPE result = op_dest->val & op_src->val;
+	DATA_TYPE_S result = op_dest->val & op_src->val;
 	OPERAND_W(op_dest, result);
 
 	/* TODO: Update EFLAGS. */
