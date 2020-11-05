16e7e0d6007840dc577677380ead6b12998be5a0
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-05 11:00:17 +0000

    >  run 3019244235 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:00:17 up 5 days, 23:57,  1 user,  load average: 1.65, 1.05, 0.80 755e04dbeec2e0d7ab1563c360f39d5e936f7854

--- testcase/src/bit.c
+++ testcase/src/bit.c
@@ -23,19 +23,19 @@ int main() {
 	uint8_t buf[2];
 
 	buf[0] = 0xaa; 
-	set_bp();
-	nemu_assert(getbit(buf, 0) == 0);set_bp();
-	nemu_assert(getbit(buf, 1) == 1);set_bp();
+
+	nemu_assert(getbit(buf, 0) == 0);
+	nemu_assert(getbit(buf, 1) == 1);
 	nemu_assert(getbit(buf, 2) == 0);
 	nemu_assert(getbit(buf, 3) == 1);
 	nemu_assert(getbit(buf, 4) == 0);
 	nemu_assert(getbit(buf, 5) == 1);
 	nemu_assert(getbit(buf, 6) == 0);
 	nemu_assert(getbit(buf, 7) == 1);
-	set_bp();
+	
 	setbit(buf, 8, 1);
 	set_bp();
-	setbit(buf, 9, 0);
+	setbit(buf, 9, 0);set_bp();
 	setbit(buf, 10, 1);
 	setbit(buf, 11, 0);
 	setbit(buf, 12, 1);
