d234eda426cc243868e9ec871954717e89de3d8c
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-05 10:41:02 +0000

    >  run 3019244235 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:41:02 up 5 days, 23:38,  1 user,  load average: 1.01, 0.78, 0.63 fd25f921cf4a9c3ff2efb4d5eb6b704b3d3726a1

--- testcase/src/bit.c
+++ testcase/src/bit.c
@@ -31,7 +31,7 @@ int main() {
 	nemu_assert(getbit(buf, 5) == 1);
 	nemu_assert(getbit(buf, 6) == 0);
 	nemu_assert(getbit(buf, 7) == 1);
-
+	set_bp();
 	setbit(buf, 8, 1);
 	set_bp();
 	setbit(buf, 9, 0);
