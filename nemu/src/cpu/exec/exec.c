6e59e6d292f271961e65da826590580e0ed17d0d
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-05 14:55:53 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:55:53 up 6 days,  3:52,  1 user,  load average: 0.71, 0.64, 0.69 6c7faacba6a14e37224e95b2de835478a03d1bd

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -65,12 +65,12 @@ make_group(group2_cl_v,
 /* 0xf6 */
 make_group(group3_b,
 	inv, inv, not_rm_b, inv, 
-	inv, imul_rm2a_b, inv, inv)
+	inv, inv, inv, inv)
 
 /* 0xf7 */
 make_group(group3_v,
 	inv, inv, not_rm_v, inv, 
-	inv, imul_rm2a_v, inv, inv)
+	inv, inv, inv, inv)
 
 /* 0xfe */
 make_group(group4,
@@ -120,12 +120,12 @@ helper_fun opcode_table [256] = {
 /* 0x5c */	pop_r_v, pop_r_v, pop_r_v, pop_r_v,
 /* 0x60 */	inv, inv, inv, inv,
 /* 0x64 */	inv, inv, operand_size, inv,
-/* 0x68 */	inv, imul_i_rm2r_v, inv, imul_si_rm2r_v,
+/* 0x68 */	inv, inv, inv, inv,
 /* 0x6c */	inv, inv, inv, inv,
 /* 0x70 */	inv, inv, inv, inv,
 /* 0x74 */	je_i_b, jne_i_b, jbe_i_b, inv,
 /* 0x78 */	inv, inv, inv, inv,
-/* 0x7c */	inv, inv, jle_i_b, jg_i_b,
+/* 0x7c */	inv, inv, jle_i_b, jg_i_v,
 /* 0x80 */	group1_b, group1_v, inv, group1_sx_v, 
 /* 0x84 */	test_r2rm_b, test_r2rm_v, inv, inv,
 /* 0x88 */	mov_r2rm_b, mov_r2rm_v, mov_rm2r_b, mov_rm2r_v,
@@ -204,7 +204,7 @@ helper_fun _2byte_opcode_table [256] = {
 /* 0xa0 */	inv, inv, inv, inv, 
 /* 0xa4 */	inv, inv, inv, inv,
 /* 0xa8 */	inv, inv, inv, inv,
-/* 0xac */	inv, inv, inv, imul_rm2r_v,
+/* 0xac */	inv, inv, inv, inv,
 /* 0xb0 */	inv, inv, inv, inv, 
 /* 0xb4 */	inv, inv, movzb_v, movzw_l, 
 /* 0xb8 */	inv, inv, inv, inv,
