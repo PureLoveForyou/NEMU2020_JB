a01f29cb2eaf122a3e96ebc04c8ef4b66c9740d0
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-08 13:19:06 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:19:06 up  9:38,  1 user,  load average: 1.11, 0.65, 0.60 e12af7b5268059f4c1145d0fff243283395da315

--- nemu/src/cpu/exec/logic/shr-template.h
+++ nemu/src/cpu/exec/logic/shr-template.h
@@ -13,7 +13,8 @@ static void do_execute () {
 	/* TODO: Update EFLAGS. */
 	//panic("please implement me");
 	DATA_TYPE result = dest;
-	cpu.CF = MSB(src << ((DATA_BYTE << 3) - 1));
+	//cpu.CF = MSB(src << ((DATA_BYTE << 3) - 1));
+	cpu.CF = 0;
     cpu.ZF = !result;
     cpu.SF = MSB(result);//get sign flag
     /*judge whether number of 1 in low 8 bits is even*/
