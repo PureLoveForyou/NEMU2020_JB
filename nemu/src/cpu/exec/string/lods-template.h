09c71160f3875932a507fca5c7b98d5f93077637
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-08 05:26:15 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:26:15 up  1:45,  1 user,  load average: 0.97, 0.65, 0.52 f563a2e4937316e06ab72e0a68654ee0836e5c4f

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -3,13 +3,13 @@
 #define instr lods
 
 make_helper(concat(lods_n_, SUFFIX)) {
-        REG(R_EAX) = MEM_R(REG(R_ESI));
+        REG(R_EAX) = MEM_R(reg_l(R_ESI));
     /*update esi*/
     if(cpu.DF == 0)
-        REG(R_ESI) += DATA_BYTE;
+        reg_l(R_ESI) += DATA_BYTE;
     else
-        REG(R_ESI) -= DATA_BYTE;
-    print_asm("lods");
+        reg_l(R_ESI) -= DATA_BYTE;
+    print_asm("lods%s", str(SUFFIX));
     return 1;
 }
 
