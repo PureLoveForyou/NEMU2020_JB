b3dff115be2fa83e6e053bf0532a2b29906a262d
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-08 08:19:37 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:19:37 up  4:38,  1 user,  load average: 0.65, 0.59, 0.58 10d54c3c1761f169e0f298bd451fa1a716774e80

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -5,14 +5,14 @@
 make_helper(concat(movs_n_, SUFFIX)) {
     MEM_W(REG(R_EDI), MEM_R(REG(R_ESI)));
     if(cpu.DF == 0) {
-        REG(R_EDI) += DATA_BYTE;
-        REG(R_ESI) += DATA_BYTE;
+        reg_l(R_EDI) += DATA_BYTE;
+        reg_l(R_ESI) += DATA_BYTE;
     }
     else {
-        REG(R_EDI) -= DATA_BYTE;
-        REG(R_ESI) -= DATA_BYTE;
+        reg_l(R_EDI) -= DATA_BYTE;
+        reg_l(R_ESI) -= DATA_BYTE;
     }
-    print_asm("movs");
+    print_asm("movs%s", str(SUFFIX));
     return 1;
 }
 
