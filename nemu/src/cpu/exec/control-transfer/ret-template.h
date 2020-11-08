d508b8ea8a62f65c87d2570f4b4d9c41fe12fa1f
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-08 15:43:50 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:43:50 up 12:03,  1 user,  load average: 1.06, 1.40, 1.34 9118e3b8554dc258acf3526c7e42e2e440cbb0

--- nemu/src/cpu/exec/control-transfer/ret-template.h
+++ nemu/src/cpu/exec/control-transfer/ret-template.h
@@ -4,18 +4,19 @@
 
 
 make_helper(concat(ret_o_, SUFFIX)) {
-    cpu.eip = MEM_R(reg_l(R_ESP));//Get return address
-    reg_l(R_ESP) += DATA_BYTE;//update esp
+    cpu.eip = MEM_R(REG(R_ESP));//Get return address
+    REG(R_ESP) += DATA_BYTE;//update esp
     print_asm_template1();
     return 1;//lenth of instruction ret
 }
 
 make_helper(concat(ret_i_, SUFFIX)) {
-    int i, val = instr_fetch(cpu.eip + 1, 2);
+    uint32_t val = instr_fetch(cpu.eip + 1, 2);
     cpu.eip = MEM_R(REG(R_ESP));//Get return address
     if(DATA_BYTE == 2)
         cpu.eip &= 0xffff;
     REG(R_ESP) += DATA_BYTE;//update esp
+    int i;
     for(i = 0; i < val; i += DATA_BYTE)
         MEM_W(REG(R_ESP) + i, 0);//pop imm16 bytes of parameters
     REG(R_ESP) += val;//update esp
