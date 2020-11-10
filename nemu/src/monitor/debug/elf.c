8f0a6ed76e3bc3a2c1844a48f52c9f6270c1d334
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-10 13:37:36 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:37:36 up 42 min,  1 user,  load average: 1.01, 0.75, 0.71 1d986646c9b15e6d2f81dc9b16a3ff2ef6513345

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -96,10 +96,6 @@ uint32_t get_var_value(char *var, bool *success) {
 			if(strcmp(var, name) == 0)
 				return symtab[i].st_value;
 		}
-		else {
-			printf("No such variable\n");
-			assert(0);
-		}
 	}
 	*success = false;
 	return 0;
