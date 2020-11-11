f8c04c24a82bf2c3adbc5919f92b81632f6677ea
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-11 12:27:48 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:27:48 up 23:32,  1 user,  load average: 1.34, 1.00, 0.81 671450beebe74da330402a935b3a6217af51e693

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -258,10 +258,7 @@ static int cmp_bt(char *args)
 			printf("#%d %s ", num, FuncName);
 		else
 			printf("#%d 0x%08x in %s ()", num, current_ebp.ret_addr, FuncName);
-		if(strcmp(FuncName, "main") == 0)
-			printf("\n");
-		else
-			printf("arguments: arg[0]:0x%08x arg[1]:0x%08x arg[2]:0x%08x arg[3]:0x%08x\n", 
+		printf("arguments: arg[0]:0x%08x arg[1]:0x%08x arg[2]:0x%08x arg[3]:0x%08x\n", 
 				swaddr_read(current_ebp.prev_ebp + 8, 4), swaddr_read(current_ebp.prev_ebp + 12, 4), 
 				swaddr_read(current_ebp.prev_ebp + 16, 4), swaddr_read(current_ebp.prev_ebp + 20, 4));
 		current_ebp.ret_addr = swaddr_read(current_ebp.prev_ebp + 4, 4);
--- testcase/src/add-longlong.c
+++ testcase/src/add-longlong.c
@@ -2,7 +2,6 @@
 
 long long add(long long a, long long b) {
 	long long c = a + b;
-	set_bp();
 	return c;
 }
 
