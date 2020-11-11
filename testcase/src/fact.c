8bd2780d8fdefb58d538cfb3fdbc80fbd0535670
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-11 12:12:58 +0000

    >  run 3019244235 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:12:58 up 23:17,  1 user,  load average: 0.68, 0.56, 0.55 e224f223599257b9257a9e5f8f8d82ceff833a0

--- testcase/src/fact.c
+++ testcase/src/fact.c
@@ -4,6 +4,7 @@ int f[15];
 int ans[] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600};
 
 int fact(int n) {
+	set_bp();
 	if(n == 0 || n == 1) return 1;
 	else return fact(n - 1) * n;
 }
