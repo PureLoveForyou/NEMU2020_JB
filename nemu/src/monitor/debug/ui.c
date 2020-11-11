aa1ef8ce6d497c715ca8a5f780b8433a884dcd0f
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-11 11:55:44 +0000

    >  compile NEMU 3019244235 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:55:44 up 23:00,  1 user,  load average: 0.52, 0.46, 0.47 8fadb5547307d3623870da6a973d2e981264b125

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -254,11 +254,11 @@ static int cmp_bt(char *args)
 		if(FuncName[0] == '\0') {
 			printf("No function called. Please run the program first\n");break;
 		}
-		current_ebp.ret_addr = swaddr_read(current_ebp.prev_ebp + 4, 4);
 		printf("#%d 0x%08x in %s\n", num++, current_ebp.ret_addr, FuncName);
 		printf("arguments: arg[0]:0x%08x arg[1]:0x%08x arg[2]:0x%08x arg[3]:0x%08x\n", 
 				swaddr_read(current_ebp.prev_ebp + 8, 4), swaddr_read(current_ebp.prev_ebp + 12, 4), 
 				swaddr_read(current_ebp.prev_ebp + 16, 4), swaddr_read(current_ebp.prev_ebp + 20, 4));
+		current_ebp.ret_addr = swaddr_read(current_ebp.prev_ebp + 4, 4);
 		current_ebp.prev_ebp = swaddr_read(current_ebp.prev_ebp, 4);
 	}
 	return 0;
