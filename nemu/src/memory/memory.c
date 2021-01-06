75ea9fc01f5aff47c54e4c65542074fbadc8d914
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-06 22:10:56 +0800

    >  compile NEMU 3019244235 desmond Linux desmond-VirtualBox 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:10:56 up  5:54,  1 user,  load average: 1.19, 0.48, 0.49 730a4e3dc58d48024f04cc9b471cb50e766b5e0f

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,4 +1,6 @@
 #include "common.h"
+#include "memory/cache.h"
+#include "burst.h"
 
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
@@ -6,11 +8,26 @@ void dram_write(hwaddr_t, size_t, uint32_t);
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	int L1_1st_line = ReadCache_L1(addr);
+	uint32_t offset = addr & (CACHE_BLOCK_SIZE_L1 - 1);
+	uint8_t ret[BURST_LEN << 1];
+	if (offset + len > CACHE_BLOCK_SIZE_L1){
+		int L1_2nd_line = ReadCache_L1(addr + CACHE_BLOCK_SIZE_L1 - offset);
+		memcpy(ret,Cache_L1[L1_1st_line].data + offset,CACHE_BLOCK_SIZE_L1 - offset);
+		memcpy(ret + CACHE_BLOCK_SIZE_L1 - offset,Cache_L1[L1_2nd_line].data,len - (CACHE_BLOCK_SIZE_L1 - offset));
+	}else {
+		memcpy(ret,Cache_L1[L1_1st_line].data + offset,len);
+	}
+
+	int temp = 0;
+	uint32_t ans = unalign_rw(ret + temp, 4) & (~0u >> ((4 - len) << 3));
+	return ans;
+	//return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	dram_write(addr, len, data);
+	WriteCache_L1(addr, len, data);
+	//dram_write(addr, len, data);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
