04d0fd789f591209a9b0ff32608410ed101dfaa0
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-17 12:22:51 +0800

    >  compile NEMU 3019244235 desmond Linux desmond-VirtualBox 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:22:51 up  5:31,  1 user,  load average: 0.83, 0.67, 0.72 168b1aa1e0d17db6941c9658e2193f2c12f6e035

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -7,6 +7,7 @@
 
 /* Use the function to get the start address of user page directory. */
 PDE* get_updir();
+PTE vm_pt[NR_PTE] align_to_page;
 
 void create_video_mapping() {
 	/* TODO: create an identical mapping from virtual memory area 
@@ -14,7 +15,17 @@ void create_video_mapping() {
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
-	panic("please implement me");
+	//panic("please implement me");
+	PDE *pde = (PFE *)va_to_pa(get_updir());
+
+	pde[0].val = make_pde(va_to_pa(vm_pt));
+	int tot = SCR_SIZE/PAGE_SIZE;
+	if(SCR_SIZE % PAGE_SIZE != 0)
+		tot++;
+	int i;
+	for(i = VMEM_ADDR/PAGE_SIZE; i < VMEM_ADDR/PAGE_SIZE + tot; i++){
+		vm_pt[i].val = make_pte(i << 12);
+	}
 }
 
 void video_mapping_write_test() {
