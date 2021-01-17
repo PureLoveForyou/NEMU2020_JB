d66edc12f48ae066785c8183b3bb9e8ed264b225
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-17 11:48:14 +0800

    >  compile NEMU 3019244235 desmond Linux desmond-VirtualBox 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:48:14 up  4:57,  1 user,  load average: 1.31, 0.84, 0.57 f2ee74e77c50fc2dc0d010bb0e2e5a43b5f0e2

---
+++ nemu/src/cpu/exec/logic/std.c
@@ -0,0 +1,7 @@
+#include "cpu/exec/helper.h"
+
+make_helper(std){
+    cpu.DF = 1;
+    print_asm("std");
+    return 1;
+}
---
+++ nemu/src/cpu/exec/logic/std.h
@@ -0,0 +1,6 @@
+#ifndef _STD_H_
+#define _STD_H_
+
+make_helper(std);
+
+#endif
